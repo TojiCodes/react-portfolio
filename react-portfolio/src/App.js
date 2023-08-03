1796147131885228aca500ad10803a61ed672f5d
/\///\/_//\ _ <tojicodes@gmail.com>
2023-08-03 17:09:48 -0500

    updated app.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Project from "./pages/Project";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/resume" component={Resume} />
+          <Route path="/project/:id" component={Project} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
